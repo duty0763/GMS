5a500c05dea6ed5adb2f8c667850a5034c3ea3ca
Duty0763 <1211062118@qq.com>
2020-06-07 21:34:12 +0800

    解决filter拦截CSS问题

--- src/com/LoginFilter.java
+++ src/com/LoginFilter.java
@@ -18,19 +18,25 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         // 写入登录认证业务逻辑
 
-        HttpServletResponse response=(HttpServletResponse)servletResponse;
-        HttpServletRequest request=(HttpServletRequest)servletRequest;
-        HttpSession session=request.getSession();
-        Object object=session.getAttribute("userName");//获取session中作为判断的字段
-        String path=request.getServletPath();  //获取路径 request.getServletPath()返回：/main/list.jsp
-        // String path = req.getRequestURI();// 获得用户请求的URI， request.getRequestURI() 返回：项目名/main/list.jsp
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        Object object = session.getAttribute("userName");//获取session中作为判断的字段
+        String path = request.getServletPath();  //获取路径 request.getServletPath()返回：/main/list.jsp
+        String url = request.getRequestURI();// 获得用户请求的URI， request.getRequestURI() 返回：项目名/main/list.jsp
 
-        if(object==null&&path.indexOf("login")<0){
+
+        if (url.contains(".css") || url.contains(".js") || url.contains(".png") || url.contains(".jpg")) {
+            //如果发现是css或者js文件，直接放行
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+
+        if (object == null && path.indexOf("login") < 0) {
             //判断请求的 路径中是否包含了 登录页面的请求
             //如果包含了，那么不过滤 继续执行操作
             //如果不是login.jsp进行过滤
             response.sendRedirect("login.jsp");
-        }else{
+        } else {
             filterChain.doFilter(servletRequest, servletResponse); // 已经登陆,继续此次请求
         }
     }
